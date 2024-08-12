89ce3599e6fb9b9ea8d0a1caa87c46b77ca99c25
Ahmed Sayed Ahmed Soliman <165445727+Ahmeddsayedd@users.noreply.github.com>
2024-08-12 18:36:00 +0300

    Add solutions for Chapter 1

---
+++ Chapter 1/Ch1.cpp
@@ -0,0 +1,27 @@
+// 1.4
+
+// a) input unit
+// b) programming
+// c) Assembly language
+// d) Output unit
+// e) Memory and storage
+// f) Arithmetic and logic unit (ALU)
+// g) Arithmetic and logic unit (ALU)
+// h) High-level languages
+// i) Machine language
+// j) Control unit
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 1.5
+
+// a) C Language
+// b) C++ Language
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// 1.6
+
+// a) edit, pre-process, compile, link, load, and execute.
+// b) Integrated Development Environment (IDE)
+
